fd63e88a47dbfcb7dbdbe86ea585d887efec11a9
Victor <victoraweb@gmail.com>
2017-07-14 16:16:22 -0300

    [FEAT] Messages on connect and disconnect

--- server/app.js
+++ server/app.js
@@ -1,11 +1,23 @@
-const path = require('path');
-const express = require('express');
-const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000;
+const path       = require('path');
+const express    = require('express');
+const http       = require('http');
+const socketIO   = require('socket.io');
 
-const app = express();
+const publicPath = path.join(__dirname, '../public');
+const port       = process.env.PORT || 3000;
+
+const app        = express();
+const server     = http.createServer(app);
+const io         = socketIO(server);
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+    console.log('New user connected.');
 
-app.listen(port, () => console.log(`Running on port ${port}`));
+    socket.on('disconnect', () => {
+        console.log('User disconnected!');
+    })
+});
+
+server.listen(port, () => console.log(`Running on port ${port}`));
