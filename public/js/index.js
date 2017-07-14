28cbca5f6e086de024e5a94f01996f681e8f27b0
Victor <victoraweb@gmail.com>
2017-07-14 17:41:25 -0300

    [FEAT] Adding timestamp to message and broadcasting

--- public/js/index.js
+++ public/js/index.js
@@ -2,8 +2,6 @@ const socket = io();
 
 socket.on('connect', function() {
     console.log('I am connected to server!');
-
-    socket.emit('createMessage', { from: 'client', content: 'This is a message from client to server.' });
 });
 
 socket.on('disconnect', function() {
